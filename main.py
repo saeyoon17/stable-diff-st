a5adcb5a53484505c64b3516e5490c04eb03a30e
Saeyoon Oh <greentea@Saeyoons-MacBook-Air.local>
2023-05-25 21:23:12 +0900

    add file

---
+++ main.py
@@ -0,0 +1,26 @@
+import time
+import torch
+import streamlit as st
+import torch
+from diffusers import StableDiffusionPipeline
+
+st.set_page_config(layout="wide")
+st.title("[VESSL AI] Stable Diffusion")
+
+st.text("Loading Stable Diffusion Model")
+pipe = StableDiffusionPipeline.from_pretrained(
+    "runwayml/stable-diffusion-v1-5", torch_dtype=torch.float16
+)
+pipe = pipe.to("cuda")
+
+with st.form("prompt", clear_on_submit=False):
+    prompt = st.text_area("Prompt: ", value="")
+    submit_button = st.form_submit_button(label="Enter")
+    if submit_button:
+        image = pipe(prompt).images[0]
+
+    if submit_button:
+        col1, col2, col3 = st.columns(3)
+
+        with col2:
+            st.image(image)
